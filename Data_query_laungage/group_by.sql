35584d4b582e3be02c3ded2c3a8b3945ed3c679a
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-08-28 23:35:45 +0530

    group by expression - having

--- Data_query_laungage/group_by.sql
+++ Data_query_laungage/group_by.sql
@@ -6,10 +6,34 @@
 --           from table_name
 --           where <filter-condition>
 --           group by column_name/expression
--- order of execution: from
---                     where (row by row)
---                     group by (row by row)
---                     select (group by group)
+
+order of execution: from
+                    where (row by row)
+                    group by (row by row)
+                    select (group by group)
 
 select count(*),deptno from emp 
 group by dno;
+
+-- Group by expression: The column name/ expression which we have pass into group by class and same column name / expression we can pass with multi row functions as a group expressioninto select clause
+
+-- Having: It is used to filter the groups 
+--       - It executes group by group 
+--       - It executes after the group by clause
+--       - Can pass multiple row function in having clause
+--       - Can not use having clause without group by clause
+--       - Select group function/group by expression
+--         from table_name
+--         where <filter-condition>
+--         group by column_name/expression
+--         having <group filter condition>
+
+order of execution: from
+                    where (row by row)
+                    group by (row by row)
+                    having (group by group)
+                    select (group by group)
+
+select count(*),deptno from emp 
+group by deptno 
+having count(deptno)>4;
