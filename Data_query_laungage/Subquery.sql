698ac793ecca69e9c9fb485bf174bcc88e25cfb9
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-02 21:23:12 +0530

    types of subquery, all and any subquery operator, nested subquery

--- Data_query_laungage/Subquery.sql
+++ Data_query_laungage/Subquery.sql
@@ -11,3 +11,46 @@ where sal>(select sal from emp
 select dname from dept
 where deptno=(select deptno from emp 
               where ename='SMITH');
+
+-- Types of subquery:
+--      1.Single row subquery
+--      2.Multi row subquery
+
+-- Single row subquery: Subquery that return exactly single value 
+--                    - can use normal(=) or special(in) operator
+
+select ename from emp 
+where deptno in (select deptno from emp 
+                 where ename='JAMES');
+
+-- Multi row subquery: Subquery returns more than one value 
+--                   - can not use normal operator only special operator
+
+select ename from emp 
+where deptno in( select deptno from emp
+                 where ename='ALLEN');
+
+-- Subquery operator: All
+--                    Any
+--                    Exist
+--                    Not Exist
+
+-- All: Special operator that can be used along with relational operator and 
+--      it return true when all the conditions are satisfied
+
+select ename from emp 
+where sal> all(select sal from emp 
+               where ename='ALLEN');
+
+-- Any: Special operator that can be used along with relational operator and
+--      it return True if any one condition is satisfied
+
+select ename from emp
+where sal< Any(select sal from emp
+               where ename='ALLEN');
+
+-- Nested subquery: A query written inside the subquery 
+--                - we can nest upto 255 subquery
+
+select max(sal) from emp 
+where sal<(select max(sal) from emp) ;
