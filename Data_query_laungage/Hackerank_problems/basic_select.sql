58007671ee034063d5a3a91614a5171b815d5bc6
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-14 20:53:13 +0530

    hackerrank problem

--- Data_query_laungage/Hackerank_problems/basic_select.sql
+++ Data_query_laungage/Hackerank_problems/basic_select.sql
@@ -43,3 +43,83 @@ FROM (SELECT CITY, LENGTH(CITY) AS LEN
 FROM STATION
 ORDER BY LENGTH(CITY) DESC, CITY ASC)
 WHERE ROWNUM = 1;
+
+
+select * from city where countrycode='USA' and population>100000;
+
+select name from city where countrycode='USA' and population>120000;
+
+select * from city where id='1661';
+
+select * from city where countrycode='JPN';
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE='JPN';
+
+SELECT CITY,STATE FROM STATION;
+
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION ;
+
+select distinct city from station where 
+city like 'A%' or 
+city like 'E%' or 
+city like 'I%' or 
+city like 'O%' or 
+city like 'U%';
+
+select distinct city from station where 
+city like '%a' or 
+city like '%e' or
+city like '%i' or
+city like '%o' or
+city like '%u' ;
+
+select distinct city from station where city like 'A%a'or city like 'A%e'or city like 'A%i'or city like 'A%o'or city like 'A%u'or city like 'E%a'or city like 'E%e'or city like 'E%i'or city like 'E%o'or city like 'E%u'or city like 'I%a'or city like 'I%e'or city like 'I%i'or city like 'I%o'or city like 'I%u'or city like 'O%a'or city like 'O%e'or city like 'O%i'or city like 'O%o'or city like 'O%u'or city like 'U%a'or city like 'U%e'or city like 'U%i'or city like 'U%o'or city like 'U%u';
+or 
+select distinct city from station where substr(city,1,1)in('A','E','I','O','U') and substr(city,-1,1) in ('a','e','i','o','u');
+
+select DISTINCT city from station where substr(city,1,1) not in ('A','E','I','O','U');
+
+SELECT distinct CITY FROM STATION WHERE SUBSTR(CITY,-1,1) NOT IN('a','e','i','o','u');
+
+select distinct city from station WHERE
+upper(substr(city,1,1)) not in ('A','E','I','O','U') or
+upper(substr(city,-1,1)) not in ('A','E','I','O','U');
+
+select distinct city from station WHERE upper(substr(city,1,1)) not in ('A','E','I','O','U') and
+upper(substr(city,-1,1)) not in ('A','E','I','O','U');
+
+select name from students where marks>75
+order by substr(name,-3,3) asc,id asc;
+
+
+select name from employee 
+order by name asc;
+
+
+select name from employee where
+salary>2000 
+and 
+months<10
+order by employee_id asc;
+
+select sum(city.population) 
+from city , country where 
+city.countrycode=country.code and continent='Asia';
+
+select city.name from city,country WHERE
+city.countrycode =  country.code and country.continent='Africa';
+
+SELECT country.continent, trunc(avg(city.population)) FROM
+city,country where city.countrycode=country.code
+GROUP by country.continent;
+
+select name||'('||substr(occupation,1,1)||')'
+from occupations 
+order by name asc; 
+select 
+'There are a total of '|| count(*)|| ' ' || lower(occupation) || 's.'
+ from occupations
+ group by occupation
+ order by count(*) asc,occupation asc;
