3d614411f645ef0e49637658ab4af49f21ba7886
Keerthanagowda8 <gowdakeerthana814@gmail.com>
2025-09-25 19:16:53 +0530

    functional dependency

---
+++ functional_dependency.sql
@@ -0,0 +1,12 @@
+-- Functional dependency : There exists a dependency such that an attribute in a relation determines another attribute
+-- Types of functional dependecy:
+--          Total Functional dependency
+--          Partial Functional dependency
+--          Transitive Functional dependency 
+
+-- Total Functional dependency : All attributes are dependent on single attribute 
+-- 
+
+-- Partial Functional dependency : There exits a dependency such that part of composite key determines another attribute uniquely
+
+-- Transitive Functional dependency : There exists a dependency such that an attribute is determined by non-key attribute which in turn determined by a key attribute
